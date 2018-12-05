600813ddd21b4825b55061686705693521a9bd07
tomekskuta <tomskuta@gmail.com>
2018-12-05 21:21:04 +0100

    Updates

---
+++ precache-manifest.9820f1a0579d99eefcee8a22b226a418.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a486ce6aaabccdd1a882",
+    "url": "/loop-it/static/js/runtime~main.a486ce6a.js"
+  },
+  {
+    "revision": "289a6a62999b1be1d21f",
+    "url": "/loop-it/static/js/main.289a6a62.chunk.js"
+  },
+  {
+    "revision": "706d7215e308dcd2df32",
+    "url": "/loop-it/static/js/1.706d7215.chunk.js"
+  },
+  {
+    "revision": "d88cdb17c97c81d27e7c5aab1e4dba29",
+    "url": "/loop-it/index.html"
+  }
+];
